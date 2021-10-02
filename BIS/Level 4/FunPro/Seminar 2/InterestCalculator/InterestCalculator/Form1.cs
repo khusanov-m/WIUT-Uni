2699060924d3a2d9f89184951851d4a09b1fab45
PC <khusanov.it.tech@gmail.com>
2021-10-02 19:34:27 +0500

    feat: interest rate calculator

---
+++ BIS/Level 4/FunPro/Seminar 2/InterestCalculator/InterestCalculator/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterestCalculator
+{
+    public partial class button : Form
+    {
+        public button()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            string name = tbx_name.Text;
+            decimal deposit = num_deposit.Value;
+            decimal interest = num_interest.Value;
+            decimal result = deposit + (deposit * interest / 100);
+
+            MessageBox.Show("Hello, " + name + "\n" + "\n" + "Your deposit is " + deposit + " and your interest rate is " + interest 
+                + ". As a result, your overall amount is " + result + ".");
+        }
+    }
+}
